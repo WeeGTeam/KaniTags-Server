837026d0991ae638375fd1c418a0cbe8242ba498
Joopon <joopon.jnw@gmail.com>
2026-05-03 14:02:42 +0200

    Improve naming

--- outgoing/fs/src/fs_image_repository.rs
+++ outgoing/fs/src/fs_image_repository.rs
@@ -19,24 +19,24 @@ impl FsImageRepository {
         return FsImageRepository { lib_path: library_path };
     }
 
-    async fn get_library_path(&self) -> Result<&Path> {
+    async fn get_library_directory(&self) -> Result<&Path> {
         ensure_directory_exists(&self.lib_path).await?;
         Ok(self.lib_path.as_path())
     }
 
-    async fn get_thumbnails_path(&self, options: ThumbnailOptions) -> Result<PathBuf> {
-        let thumbnails_path = self.lib_path.join("thumbnails");
-        let gallery_thumbnails_path = thumbnails_path.join(get_directory_name(&options));
-        ensure_directory_exists(&gallery_thumbnails_path).await?;
-        Ok(gallery_thumbnails_path)
+    async fn get_thumbnail_directory(&self, options: ThumbnailOptions) -> Result<PathBuf> {
+        let common_thumbnails_dir = self.lib_path.join("thumbnails");
+        let thumbnail_dir = common_thumbnails_dir.join(get_thumbnail_directory_name(&options));
+        ensure_directory_exists(&thumbnail_dir).await?;
+        Ok(thumbnail_dir)
     }
 }
 
 #[async_trait]
 impl ImageRepository for FsImageRepository {
     async fn store_image(&self, image: PantsuImage, file_content: Bytes) -> Result<()> {
-        let library_path = self.get_library_path().await?;
-        let path = library_path.join(image.filename());
+        let library_dir = self.get_library_directory().await?;
+        let path = library_dir.join(image.filename());
 
         let mut file = OpenOptions::new()
             .write(true)
@@ -63,8 +63,8 @@ impl ImageRepository for FsImageRepository {
         file_content: Bytes,
         options: ThumbnailOptions
     ) -> Result<()> {
-        let thumbnails_path = self.get_thumbnails_path(options).await?;
-        let path = thumbnails_path.join(image.filename_with_custom_extension(ImageFormat::JPG));
+        let thumbnail_dir = self.get_thumbnail_directory(options).await?;
+        let path = thumbnail_dir.join(image.filename_with_custom_extension(ImageFormat::JPG));
         
         let mut file = OpenOptions::new()
             .write(true)
@@ -99,6 +99,6 @@ async fn ensure_directory_exists(directory: &Path) -> Result<()> {
         })
 }
 
-fn get_directory_name(options: &ThumbnailOptions) -> String {
+fn get_thumbnail_directory_name(options: &ThumbnailOptions) -> String {
     format!("{}x{}", options.max_size, options.max_size)
 }
