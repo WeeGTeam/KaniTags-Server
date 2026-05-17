84e2d0c5a5c6e469ac1bd8ce529022efa22d0525
Joris Diem <j.diem@me.com>
2026-05-17 22:21:09 +0200

    add login service to app state

--- incoming/http-api/src/lib.rs
+++ incoming/http-api/src/lib.rs
@@ -1,6 +1,7 @@
 use crate::router::{AppState, OpenApiRouter};
 use axum::extract::DefaultBodyLimit;
 use kani_domain_api_incoming::image_management::ImageManagementService;
+use kani_domain_api_incoming::login_service::LoginService;
 use pantsu_domain::common::error::Error;
 use pantsu_domain::common::result::Result;
 use pantsu_domain::reverse_image_search::ReverseImageSearchService;
@@ -12,21 +13,27 @@ use tower_http::trace::TraceLayer;
 mod request_id;
 pub mod router;
 
-pub async fn launch_server<RS, IS>(
+pub async fn launch_server<RS, IS, LS>(
     reverse_image_search_service: Arc<RS>,
     image_management_service: Arc<IS>,
+    login_service: Arc<LS>,
     request_body_limit: usize,
     server_port: u16,
 ) -> Result<()>
 where
     RS: ReverseImageSearchService + Send + Sync + 'static,
     IS: ImageManagementService + Send + Sync + 'static,
+    LS: LoginService + Send + Sync + 'static,
 {
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", server_port))
         .await
         .map_err(|_| Error::TodoError())?;
 
-    let shared_state = AppState::new(reverse_image_search_service, image_management_service);
+    let shared_state = AppState::new(
+        reverse_image_search_service,
+        image_management_service,
+        login_service,
+    );
     let body_limit = DefaultBodyLimit::max(request_body_limit);
     let router = server::new(OpenApiRouter(shared_state))
         .layer(TraceLayer::new_for_http().make_span_with(request_id::request_id_tracing_span))
@@ -34,7 +41,8 @@ where
         .layer(PropagateRequestIdLayer::x_request_id())
         .layer(body_limit);
 
-    axum::serve(listener, router).await
+    axum::serve(listener, router)
+        .await
         .map_err(|_| Error::TodoError())?;
 
     Ok(())
--- incoming/http-api/src/router/mod.rs
+++ incoming/http-api/src/router/mod.rs
@@ -1,4 +1,5 @@
 use kani_domain_api_incoming::image_management::ImageManagementService;
+use kani_domain_api_incoming::login_service::LoginService;
 use pantsu_domain::common::error::Error;
 use pantsu_domain::reverse_image_search::ReverseImageSearchService;
 use pantsu_openapi::apis::ErrorHandler;
@@ -24,18 +25,24 @@ impl ErrorHandler<Error> for AppState {}
 pub struct AppState {
     pub reverse_image_search_service: Arc<dyn ReverseImageSearchService + Send + Sync>,
     pub image_management_service: Arc<dyn ImageManagementService + Send + Sync>,
+    pub login_service: Arc<dyn LoginService + Send + Sync>,
 }
 
 impl AppState {
-    pub fn new<RS, IS>(iqdb_service: Arc<RS>, image_management_service: Arc<IS>,
+    pub fn new<RS, IS, LS>(
+        iqdb_service: Arc<RS>,
+        image_management_service: Arc<IS>,
+        login_service: Arc<LS>,
     ) -> Self
     where
         RS: ReverseImageSearchService + Send + Sync + 'static,
         IS: ImageManagementService + Send + Sync + 'static,
+        LS: LoginService + Send + Sync + 'static,
     {
         Self {
             reverse_image_search_service: iqdb_service,
             image_management_service,
+            login_service,
         }
     }
 }
