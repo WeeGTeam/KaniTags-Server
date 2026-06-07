31896a6af8e6a813b319d6d9fe7186ba20354394
Joopon <joopon.jnw@gmail.com>
2026-06-07 17:35:54 +0200

    Copy ToDomain/FromDomain from http api
    Has to be duplicated to allow crate to
    implement the trait on structs defined
    outside the crate.

---
+++ outgoing/postgres/src/database/converter.rs
@@ -0,0 +1,101 @@
+pub trait ToDomain<D> {
+    fn to_domain(self) -> D;
+}
+
+pub trait TryToDomain<D> {
+    type Error;
+
+    fn try_to_domain(self) -> Result<D, Self::Error>;
+}
+
+pub trait FromDomain<D> {
+    fn from_domain(value: D) -> Self;
+}
+
+pub trait TryFromDomain<D>: Sized {
+    type Error;
+
+    fn try_from_domain(value: D) -> Result<Self, Self::Error>;
+}
+
+// impl
+
+impl<A, D> ToDomain<Vec<D>> for Vec<A>
+where
+    A: ToDomain<D>,
+{
+    fn to_domain(self) -> Vec<D> {
+        self.into_iter().map(A::to_domain).collect()
+    }
+}
+
+impl<A, D> ToDomain<Option<D>> for Option<A>
+where
+    A: ToDomain<D>,
+{
+    fn to_domain(self) -> Option<D> {
+        self.map(A::to_domain)
+    }
+}
+
+impl<A, D> TryToDomain<Vec<D>> for Vec<A>
+where
+    A: TryToDomain<D>,
+{
+    type Error = A::Error;
+
+    fn try_to_domain(self) -> Result<Vec<D>, Self::Error> {
+        self.into_iter().map(A::try_to_domain).collect()
+    }
+}
+
+impl<A, D> TryToDomain<Option<D>> for Option<A>
+where
+    A: TryToDomain<D>,
+{
+    type Error = A::Error;
+
+    fn try_to_domain(self) -> Result<Option<D>, Self::Error> {
+        self.map(A::try_to_domain).transpose()
+    }
+}
+
+impl<A, D> FromDomain<Vec<D>> for Vec<A>
+where
+    A: FromDomain<D>,
+{
+    fn from_domain(value: Vec<D>) -> Self {
+        value.into_iter().map(A::from_domain).collect()
+    }
+}
+
+impl<A, D> FromDomain<Option<D>> for Option<A>
+where
+    A: FromDomain<D>,
+{
+    fn from_domain(value: Option<D>) -> Self {
+        value.map(A::from_domain)
+    }
+}
+
+impl<A, D> TryFromDomain<Vec<D>> for Vec<A>
+where
+    A: TryFromDomain<D>,
+{
+    type Error = A::Error;
+
+    fn try_from_domain(value: Vec<D>) -> Result<Self, Self::Error> {
+        value.into_iter().map(A::try_from_domain).collect()
+    }
+}
+
+impl<A, D> TryFromDomain<Option<D>> for Option<A>
+where
+    A: TryFromDomain<D>,
+{
+    type Error = A::Error;
+
+    fn try_from_domain(value: Option<D>) -> Result<Self, Self::Error> {
+        value.map(A::try_from_domain).transpose()
+    }
+}
--- outgoing/postgres/src/database/mod.rs
+++ outgoing/postgres/src/database/mod.rs
@@ -1,6 +1,7 @@
 use crate::Postgres;
 use kani_domain_api_outgoing::database::Database;
 
+pub mod converter;
 pub mod image_database;
 pub mod similarity_database;
 pub mod user_database;
