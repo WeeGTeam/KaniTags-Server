317df6baeeac2b065085e7f26be379c910e30e70
Joopon <joopon.jnw@gmail.com>
2026-07-26 01:05:54 +0200

    Fix naming after "Dto" suffix was added in openapi

--- incoming/http-api/src/converter/import.rs
+++ incoming/http-api/src/converter/import.rs
@@ -1,8 +1,9 @@
 use crate::converter::FromDomain;
-use kani_openapi::models::ImportSession;
+use kani_domain_api_model::import::ImportSession;
+use kani_openapi::models::ImportSessionDto;
 
-impl FromDomain<kani_domain_api_model::import::ImportSession> for ImportSession {
-    fn from_domain(session: kani_domain_api_model::import::ImportSession) -> Self {
+impl FromDomain<ImportSession> for ImportSessionDto {
+    fn from_domain(session: ImportSession) -> Self {
         Self {
             id: session.id.to_string(),
             created_at: session.created_at,
--- incoming/http-api/src/converter/tag_converter.rs
+++ incoming/http-api/src/converter/tag_converter.rs
@@ -1,7 +1,8 @@
 use crate::converter::{FromDomain, ToDomain};
 use kani_domain_api_model::tag::{Tag, TagType};
+use kani_openapi::models::{TagDto, TagTypeDto};
 
-impl FromDomain<Tag> for kani_openapi::models::Tag {
+impl FromDomain<Tag> for TagDto {
     fn from_domain(tag: Tag) -> Self {
         Self {
             id: tag.id.to_string(),
@@ -11,26 +12,26 @@ impl FromDomain<Tag> for kani_openapi::models::Tag {
     }
 }
 
-impl ToDomain<TagType> for &kani_openapi::models::TagType {
+impl ToDomain<TagType> for &TagTypeDto {
     fn to_domain(self) -> TagType {
         match self {
-            kani_openapi::models::TagType::Rating => TagType::Rating,
-            kani_openapi::models::TagType::Artist => TagType::Artist,
-            kani_openapi::models::TagType::Source => TagType::Source,
-            kani_openapi::models::TagType::Character => TagType::Character,
-            kani_openapi::models::TagType::General => TagType::General,
+            TagTypeDto::Rating => TagType::Rating,
+            TagTypeDto::Artist => TagType::Artist,
+            TagTypeDto::Source => TagType::Source,
+            TagTypeDto::Character => TagType::Character,
+            TagTypeDto::General => TagType::General,
         }
     }
 }
 
-impl FromDomain<TagType> for kani_openapi::models::TagType {
+impl FromDomain<TagType> for TagTypeDto {
     fn from_domain(tag_type: TagType) -> Self {
         match tag_type {
-            TagType::Rating => kani_openapi::models::TagType::Rating,
-            TagType::Artist => kani_openapi::models::TagType::Artist,
-            TagType::Source => kani_openapi::models::TagType::Source,
-            TagType::Character => kani_openapi::models::TagType::Character,
-            TagType::General => kani_openapi::models::TagType::General,
+            TagType::Rating => TagTypeDto::Rating,
+            TagType::Artist => TagTypeDto::Artist,
+            TagType::Source => TagTypeDto::Source,
+            TagType::Character => TagTypeDto::Character,
+            TagType::General => TagTypeDto::General,
         }
     }
 }
